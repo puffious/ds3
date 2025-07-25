4bc137734aa4abfc2bfdc4600e258dd8ca87252d
puffious <eightpuff@proton.me>
2025-07-25 19:59:37 +0000

    stuff

--- lab1/2matrix.c
+++ lab1/2matrix.c
@@ -4,28 +4,67 @@
 int rowC = 3; // row count
 int colC = 3; // column count
 
-int additionOf(int m1[rowC][colC], int m2[rowC][colC]){
-    // return addition of matrix m1 & m2
-    int m3[rowC][colC];
+int additionOf(int m1[rowC][colC], int m2[rowC][colC], int m3[rowC][colC]){
     for (int r = 0; r < rowC; r++){
         for (int c = 0; c < colC; c++){
             m3[r][c] = m1[r][c] + m2[r][c]; 
         }
     }
-    return m3;
 }
 
-int substractionOf(int m1[rowC][colC], int m2[rowC][colC]){
-    // return substraction of matrix m1 & m2
-    int m3[rowC][colC];
+int substractionOf(int m1[rowC][colC], int m2[rowC][colC], int m3[rowC][colC]){
     for (int r = 0; r < rowC; r++){
         for (int c = 0; c < colC; c++){
             m3[r][c] = m1[r][c] - m2[r][c]; 
         }
     }
-    return m3;
 }
 
-int multiplicationOf(int m1[rowC][colC], int m2[rowC][colC]){
-       
+int multiplicationOf(int m1[rowC][colC], int m2[rowC][colC], int m3[rowC][colC]){
+    for (int i = 0; i < rowC; i++){
+        for (int j = 0; j < colC; j++){
+            m3[i][j] = 0;
+            for (int k = 0; k < rowC ; k++){
+                m3[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+}
+
+int populateMatrix(int m[rowC][colC]){
+    printf("input\n");
+    for (int r = 0; r < rowC; r++){
+        for (int c = 0; c < colC; c++){
+            printf("r%d c%d: ", r, c);
+            scanf("%d", &m[r][c]);
+        }
+    }
+}
+
+int printMatrix(int m[rowC][colC]){
+    printf("output\n");
+    for (int r = 0; r < rowC; r++){
+        for (int c = 0; c < colC; c++){
+            printf("%d\t", m[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void){
+    int m1[rowC][colC], m2[rowC][colC];
+
+    populateMatrix(m1);
+    populateMatrix(m2);
+
+    int madd[rowC][colC], msub[rowC][colC], mmul[rowC][colC];
+
+    additionOf(m1, m2, madd);
+    printMatrix(madd);
+
+    substractionOf(m1, m2, msub);
+    printMatrix(msub);
+    
+    multiplicationOf(m1, m2, mmul);
+    printMatrix(mmul);
 }
---
+++ lab2/3stack.c
@@ -0,0 +1,87 @@
+// Implement a stack using arrays and perform the following operations: Push, Pop, Peep, and Display.
+
+#include <stdio.h>
+#include<stdlib.h>
+int push(int value);
+int pop();
+int display();
+int peep();
+
+int top = 0;
+int stack[10];
+
+int main(void) {
+    int keepRunning = 1, choice = 0, temp = 0;
+    char options[5][10] = {{"push"}, {"pop"}, {"peep"}, {"display"}, {"exit"}};
+    for (int i = 0; i < 5; i++) {
+        printf("%d: %s\n", i + 1, options[i]);
+    }
+
+    while (keepRunning) {
+        display();
+        printf("choice: ");
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            while(getchar() != '\n'); 
+            continue;
+        }
+        switch (choice) {
+            case 1: // push
+                printf("value: ");
+                scanf("%d", &temp);
+                push(temp);
+                break;
+
+            case 2: // pop
+                printf("removed %d\n", pop());
+                break;
+
+            case 3: // peep
+                printf("on top: %d\n", peep());
+                break;
+
+            case 4: // display
+                display();
+                break;
+
+            case 5: // exit
+                printf("Exiting...\n");
+                keepRunning = 0;
+                break;
+
+            default:
+                printf("Invalid choice, please try again.\n");
+                break;
+        }
+    }
+    return 0;
+}
+
+int push(int value){
+    stack[top] = value;
+    top ++;
+}
+
+int pop(){
+    if (top > 0) {
+        top --;
+        return stack[top];
+    }
+    else return 0;
+    
+}
+
+int peep(){
+    return stack[top-1];
+}
+
+int display(){
+    printf("stack: [");
+    for (int i = 0; i < top; i++){
+        printf("%d", stack[i]);
+        if (i != top-1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+}
